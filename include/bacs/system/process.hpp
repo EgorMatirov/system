3aa21a27d60331b54e1528342611b40847b22ed4
Aleksey Filippov <sarum9in@gmail.com>
2014-03-05 01:59:54 +0400

    process::{setup,parse_result}() implementation.

---
+++ include/bacs/system/process.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <bacs/process.pb.h>
+
+#include <yandex/contest/invoker/Process.hpp>
+#include <yandex/contest/invoker/ProcessGroup.hpp>
+
+namespace bacs{namespace system{namespace process
+{
+    void setup(
+        const yandex::contest::invoker::ProcessPointer &process,
+        const bacs::process::ResourceLimits &resource_limits);
+
+    void setup(
+        const yandex::contest::invoker::ProcessGroupPointer &process_group,
+        const yandex::contest::invoker::ProcessPointer &process,
+        const bacs::process::ResourceLimits &resource_limits);
+
+    /// \return true if process has completed successfully
+    bool parse_result(
+        const yandex::contest::invoker::ProcessGroup::Result &process_group_result,
+        const yandex::contest::invoker::Process::Result &process_result,
+        bacs::process::ExecutionResult &result);
+}}}
