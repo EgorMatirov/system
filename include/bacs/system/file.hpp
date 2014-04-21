f3e0da32c8f400887d889f0cf43b7f464ad4d23d
Aleksey Filippov <sarum9in@gmail.com>
2014-04-21 22:07:41 +0400

    file::read_{first,last}() implementation.

--- include/bacs/system/file.hpp
+++ include/bacs/system/file.hpp
@@ -10,4 +10,12 @@ namespace bacs{namespace system{namespace file
 {
     std::string read(const boost::filesystem::path &path,
                      const bacs::file::Range &range);
+
+    std::string read_first(
+        const boost::filesystem::path &path,
+        const std::uint64_t size);
+
+    std::string read_last(
+        const boost::filesystem::path &path,
+        const std::uint64_t size);
 }}}
--- src/file.cpp
+++ src/file.cpp
@@ -31,4 +31,31 @@ namespace bacs{namespace system{namespace file
         fin.close();
         return data;
     }
+
+    static std::string read(
+        const boost::filesystem::path &path,
+        const bacs::file::Range::Whence whence,
+        const std::int64_t offset,
+        const std::uint64_t size)
+    {
+        bacs::file::Range range;
+        range.set_whence(whence);
+        range.set_offset(offset);
+        range.set_size(size);
+        return read(path, range);
+    }
+
+    std::string read_first(
+        const boost::filesystem::path &path,
+        const std::uint64_t size)
+    {
+        return read(path, bacs::file::Range::BEGIN, 0, size);
+    }
+
+    std::string read_last(
+        const boost::filesystem::path &path,
+        const std::uint64_t size)
+    {
+        return read(path, bacs::file::Range::END, 0, size);
+    }
 }}}
